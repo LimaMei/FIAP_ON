9ce91bdf499ccb3142bb331a3d53f3b42c3549f6
Gabriel Lima <100251465+LimaMei@users.noreply.github.com>
2022-09-07 22:54:28 -0300

    Python

---
+++ Python/Dict_e_manipulacao/func.py
@@ -0,0 +1,20 @@
+def perguntar():
+    resposta = input("O que deseja realizar?" +
+                "<I> - Para Inserir um usuário" +
+                "<P> - Para Pesquisar um usuário" +
+                "<E> - Para Excluir um usuário" +
+                "<L> - Para Listar um usuário: ").upper()
+    return resposta
+
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
+                input("Digite a última data de acesso: "),
+                input("Qual a última estação acessada: ").upper()]
+    salvar(dicionario)
+
+
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
